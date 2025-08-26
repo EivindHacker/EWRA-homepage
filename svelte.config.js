218fc3434899287162a14f0620c46f30d2968bc7
Vebjørn Fuhre Haugholt <153077430+vebjornFH123@users.noreply.github.com>
2025-08-26 20:22:38 +0200

    Modify svelte.config.js for GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -2,18 +2,27 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-export default {
+// Bruk dette bare hvis du *ikke* har eget domene og publiserer til
+// https://USERNAME.github.io/EWRA-homepage
+const dev = process.argv.includes('dev');
+const base = dev ? '' : '/EWRA-homepage';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
   preprocess: vitePreprocess(),
   kit: {
+    // Hvis du har *eget domene*, fjern linjen under:
+    paths: { base },
+
     adapter: adapter({
       pages: 'build',
       assets: 'build',
-      fallback: '404.html',  // <- viktig for dynamiske ruter på statisk hosting (GH Pages)
-      precompress: true
+      fallback: '404.html', // GitHub Pages bruker 404.html som SPA-fallback
+      precompress: true,
+      // Viktig: la oss tillate dynamiske ruter (SPA) på statisk hosting
+      strict: false
     })
   }
 };
-
-
 
 export default config;
