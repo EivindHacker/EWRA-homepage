02e9434e02f7d65f04930d0e516ffb3692fdfa00
vebjornFH123 <vebjorn.fuhrehaugholt@gmail.com>
2025-08-26 12:57:59 +0200

    t

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			// fallback: '200.html', // bruk denne hvis du vil ha SPA-fallback
+			fallback: '200.html', // <- viktig
 			precompress: true
 		}),
 		alias: {
